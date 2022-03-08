8018b3576a7e414328617498697ffc1f5a547429
Benoit G <94395967+zebenny9616@users.noreply.github.com>
2022-03-08 11:55:23 -0500

    Calculator
    Calculator with add, sub, mult and div function.

---
+++ Calculatrice.py
@@ -0,0 +1,16 @@
+def add(n1, n2):
+    return(n1+n2)
+def sub(n1, n2):
+    return(n1-n2)
+def mult(n1, n2):
+    return(n1*n2)
+def div(n1, n2):
+    return(n1/n2)
+
+n1 = int(input('Entrez le premier chiffre: '))
+n2 = int(input('Entrez le second chiffre: '))
+
+print(add(n1, n2))
+print(sub(n1, n2))
+print(mult(n1, n2))
+print(div(n1, n2))
