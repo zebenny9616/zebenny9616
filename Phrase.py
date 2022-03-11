e4116c0be39a2c68d2b544814ab3bb7f550d3f86
Benoit G <94395967+zebenny9616@users.noreply.github.com>
2022-03-11 15:36:31 -0500

    Phrase_Complet

---
+++ Phrase.py
@@ -0,0 +1,13 @@
+def phrase_complet():
+
+    animal = input("Veuillez entrer un animal : ")
+    couleur = input("Veuillez entrez une couleur : ")
+    lieu = input("Veuillez entrez un endroit : ")
+
+    print(f"J'ai trouvé un {animal} de couleur {couleur} dans mon lieu préféré {lieu}")
+
+    return None
+
+phrase_complet()
+phrase_complet()
+phrase_complet()
