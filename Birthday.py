2d55ce2492bb2a624d4d3ce483c298db3d0ed011
Benoit G <94395967+zebenny9616@users.noreply.github.com>
2022-03-11 15:20:13 -0500

    Birthday broken
    Mois + Jour fête input

---
+++ Birthday.py
@@ -0,0 +1,8 @@
+def jour_fete int(input('Entrez votre jour de fête: '))
+def mois_fete int(input('Entrez votre mois de fête en chiffre'))
+
+if jour_fete == 7 and mois_fete == 3:
+    print('Bonne fête')
+else
+    print (" Ce n'est pas ta fête aujourd'hui : ")
+
