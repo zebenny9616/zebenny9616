0b7b33079626262392f2b90f4d52c7ac87c80050
Benoit G <94395967+zebenny9616@users.noreply.github.com>
2022-03-11 21:18:47 -0500

    Update Birthday.py
    Changed quotation marks ' to " to respect syntax.

--- Birthday.py
+++ Birthday.py
@@ -1,5 +1,5 @@
-def jour_fete int(input('Entrez votre jour de fête: '))
-def mois_fete int(input('Entrez votre mois de fête en chiffre'))
+def jour_fete int(input("Entrez votre jour de fête: "))
+def mois_fete int(input("Entrez votre mois de fête en chiffre"))
 
 if jour_fete == 7 and mois_fete == 3:
     print('Bonne fête')
