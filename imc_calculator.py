68c654435eec95a8c92104fd94b5ed86ce275688
Benoit G <94395967+zebenny9616@users.noreply.github.com>
2022-03-14 09:52:20 -0400

    Create imc_calculator.py
    Non fini

---
+++ imc_calculator.py
@@ -0,0 +1,18 @@
+def grandeur_en_metre (): 
+    int(input("Entrez votre grandeur en metre : "))
+def poids_en_kg (): 
+    int(input("Entrez votre poids en kilogramme : "))
+
+def IMC (poids_en_kg / (grandeur_en_metre)**2)) 
+
+if IMC < 18.5 :
+    IMC = str("Poids insuffisant")
+elif IMC <= 18.5 or > 25 :
+    IMC = str("Poids santé")
+elif IMC <= 25 or > 30 :
+    IMC = str("Léger excès de poids")
+elif IMC <= 30 :
+    IMC = str("Obèse")
+
+
+print(f"Vous êtes dans la catégorie d'IMC suivante : {IMC}. ") 
